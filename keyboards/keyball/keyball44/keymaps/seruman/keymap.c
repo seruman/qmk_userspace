c81e2169798baecd6d49e388b077f43e57392bef
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2025-08-05 20:02:59 +0300

    keyball44: format layers

--- keyboards/keyball/keyball44/keymaps/seruman/keymap.c
+++ keyboards/keyball/keyball44/keymaps/seruman/keymap.c
@@ -30,40 +30,45 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
     );
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  // keymap for default
-  [0] = LAYOUT_universal(
-    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSLS  ,
-    LCTL_T(KC_TAB)   , KC_A     , KC_S     , LALT_T(KC_D)     , LGUI_T(KC_F)     , KC_G     ,                KC_H     , RGUI_T(KC_J)     , LALT_T(KC_K)     , KC_L     , KC_SCLN  , RCTL_T(KC_QUOT)  ,
-    KC_LSFT  , KC_Z  , KC_X     , KC_C     , KC_V     , KC_B     ,                                           KC_N     , KC_M,        KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
-                        KC_LALT    , KC_LGUI        ,MO(4),MO(1),LT(3,KC_SPC),                  KC_ENT, MO(2)    ,_______   , _______  , KC_PSCR
-  ),
+    // Layer 0: Base layer (QWERTY)
+    [0] = LAYOUT_universal(
+        KC_ESC,         KC_Q,     KC_W,     KC_E,         KC_R,         KC_T,                                      KC_Y,     KC_U,         KC_I,         KC_O,     KC_P,     KC_BSLS,
+        LCTL_T(KC_TAB), KC_A,     KC_S,     LALT_T(KC_D), LGUI_T(KC_F), KC_G,                                      KC_H,     RGUI_T(KC_J), LALT_T(KC_K), KC_L,     KC_SCLN,  RCTL_T(KC_QUOT),
+        KC_LSFT,        KC_Z,     KC_X,     KC_C,         KC_V,         KC_B,                                      KC_N,     KC_M,         KC_COMM,      KC_DOT,   KC_SLSH,  KC_RSFT,
+                                  KC_LALT,  KC_LGUI,      MO(4),        MO(1),     LT(3,KC_SPC),         KC_ENT,   MO(2),        _______,      _______,  KC_PSCR
+    ),
 
-  [1] = LAYOUT_universal(
-    KC_GRV   ,  S(KC_1) ,S(KC_2)   , S(KC_3) , S(KC_4)  , S(KC_5)  ,                                         S(KC_6)  , S(KC_7)  , S(KC_8)  , S(KC_9)  , S(KC_0)  , KC_BSPC  ,
-    _______  ,  _______ , _______  , KC_UP   , KC_ENT   , _______  ,                                         KC_LEFT  , KC_LCBR  , KC_RCBR  , KC_LBRC  , KC_RBRC  , KC_F12   ,
-    _______  ,  KC_EQL  , KC_MINS  , KC_PLUS , _______  , _______  ,                                         _______  , _______  , _______  , KC_LPRN  , KC_RPRN  , _______  ,
-                  _______  , _______ , _______  ,         _______  , _______  ,                   KC_BSPC  , _______  , _______  , _______  , _______
-  ),
+    // Layer 1: Symbols & Navigation
+    [1] = LAYOUT_universal(
+        KC_GRV,         S(KC_1),  S(KC_2),  S(KC_3),      S(KC_4),      S(KC_5),                                   S(KC_6),  S(KC_7),      S(KC_8),      S(KC_9),  S(KC_0),  KC_BSPC,
+        _______,        _______,  _______,  KC_UP,        KC_ENT,       _______,                                   KC_LEFT,  KC_LCBR,      KC_RCBR,      KC_LBRC,  KC_RBRC,  KC_F12,
+        _______,        KC_EQL,   KC_MINS,  KC_PLUS,      _______,      _______,                                   _______,  _______,      _______,      KC_LPRN,  KC_RPRN,  _______,
+                                  _______,  _______,      _______,      _______,   _______,              KC_BSPC,  _______,  _______,      _______,      _______
+    ),
 
-  [2] = LAYOUT_universal(
-    _______  ,KC_1      , KC_2     , KC_3    , KC_4     , KC_5     ,                                         KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_BSPC  ,
-    _______  ,S(KC_SCLN), KC_4     , KC_5    , KC_6     , KC_RBRC  ,                                         KC_LEFT  , KC_DOWN  , KC_UP    , KC_RIGHT , KC_QUOT  , S(KC_2)  ,
-    _______  ,S(KC_MINS), KC_1     , KC_2    , KC_3     ,S(KC_RBRC),                                        S(KC_NUHS),S(KC_INT1), KC_EQL   ,S(KC_LBRC),S(KC_SLSH),S(KC_INT3), KC_0     , KC_DOT  , _______  ,         _______  , _______  ,                   KC_DEL   , _______  , _______       , _______  , _______
-  ),
+    // Layer 2: Numbers & Arrows
+    [2] = LAYOUT_universal(
+        _______,        KC_1,       KC_2,     KC_3,         KC_4,         KC_5,                                    KC_6,       KC_7,         KC_8,     KC_9,       KC_0,       KC_BSPC,
+        _______,        S(KC_SCLN), KC_4,     KC_5,         KC_6,         KC_RBRC,                                 KC_LEFT,    KC_DOWN,      KC_UP,    KC_RIGHT,   KC_QUOT,    S(KC_2),
+        _______,        S(KC_MINS), KC_1,     KC_2,         KC_3,         S(KC_RBRC),                              S(KC_NUHS), S(KC_INT1),   KC_EQL,   S(KC_LBRC), S(KC_SLSH), S(KC_INT3),
+                                    KC_0,     KC_DOT,       _______,      _______,   _______,            KC_DEL,   _______,    _______,      _______,  _______
+    ),
 
-  [3] = LAYOUT_universal(
-    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  , _______  ,                                        RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,                                        RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
-                  QK_BOOT  , KBC_RST  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , KBC_RST  , QK_BOOT
-  ),
+    // Layer 3: RGB & Mouse controls
+    [3] = LAYOUT_universal(
+        RGB_TOG,        AML_TO,   AML_I50,  AML_D50,      _______,      _______,                                   RGB_M_P,  RGB_M_B,      RGB_M_R,      RGB_M_SW, RGB_M_SN, RGB_M_K,
+        RGB_MOD,        RGB_HUI,  RGB_SAI,  RGB_VAI,      _______,      SCRL_DVI,                                  RGB_M_X,  RGB_M_G,      RGB_M_T,      RGB_M_TW, _______,  _______,
+        RGB_RMOD,       RGB_HUD,  RGB_SAD,  RGB_VAD,      _______,      SCRL_DVD,                                  CPI_D1K,  CPI_D100,     CPI_I100,     CPI_I1K,  _______,  KBC_SAVE,
+                                  QK_BOOT,  KBC_RST,      _______,      _______,   _______,              _______,  _______,      _______,      KBC_RST,  QK_BOOT
+    ),
 
-  [4] = LAYOUT_universal(
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                        _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                        _______  , KC_BTN1  , KC_BTN2  , KC_BTN3  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                        _______  , _______  , _______  , _______  , _______  , _______  ,
-                  _______  , _______  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
-  ),
+    // Layer 4: Mouse buttons
+    [4] = LAYOUT_universal(
+        _______,        _______,  _______,  _______,      _______,      _______,                                   _______,  _______,      _______,      _______,  _______,  _______,
+        _______,        _______,  _______,  _______,      _______,      _______,                                   _______,  KC_BTN1,      KC_BTN2,      KC_BTN3,  _______,  _______,
+        _______,        _______,  _______,  _______,      _______,      _______,                                   _______,  _______,      _______,      _______,  _______,  _______,
+                                  _______,  _______,      _______,      _______,   _______,              _______,  _______,      _______,      _______,  _______
+    ),
 };
 // clang-format on
 
