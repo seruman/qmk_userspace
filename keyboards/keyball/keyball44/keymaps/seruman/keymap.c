cdc6f3567dd549b62ca2226251d7ced8aba415b6
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2025-08-05 12:55:20 +0300

    keyball44: update home row mods

--- keyboards/keyball/keyball44/keymaps/seruman/keymap.c
+++ keyboards/keyball/keyball44/keymaps/seruman/keymap.c
@@ -33,8 +33,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
   [0] = LAYOUT_universal(
     KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSLS  ,
-    LCTL_T(KC_TAB)   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , RCTL_T(KC_QUOT)  ,
-    KC_LSFT  , KC_Z  , LALT_T(KC_X)     , KC_C     , LGUI_T(KC_V)     , KC_B     ,                           KC_N     , RGUI_T(KC_M),KC_COMM  , RALT_T(KC_DOT)   , KC_SLSH  , KC_RSFT  ,
+    LCTL_T(KC_TAB)   , KC_A     , KC_S     , LALT_T(KC_D)     , LGUI_T(KC_F)     , KC_G     ,                KC_H     , RGUI_T(KC_J)     , LALT_T(KC_K)     , KC_L     , KC_SCLN  , RCTL_T(KC_QUOT)  ,
+    KC_LSFT  , KC_Z  , KC_X     , KC_C     , KC_V     , KC_B     ,                                           KC_N     , KC_M,        KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
                         KC_LALT    , KC_LGUI        ,MO(4),MO(1),LT(3,KC_SPC),                  KC_ENT, MO(2)    ,_______   , _______  , KC_PSCR
   ),
 
